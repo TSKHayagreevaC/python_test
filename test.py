78b138b0672ac77a22b2a9ff66301af33fa499b6
THIRUMALA SRIKANTH HAYAGREEVA CHARYA <69465021+TSKHayagreevaC@users.noreply.github.com>
2021-02-05 20:20:54 +0530

    commit with advance functions and calculator app

--- test.py
+++ test.py
@@ -925,3 +925,113 @@
 #         print("ok")
 
 # Functions With Outputs
+
+# def my_function_output():
+#     return 3*2
+# output = my_function_output()
+# print(output)
+
+# def format_name(f_name, l_name):
+#     if f_name == "" or l_name == "":
+#         return "Please Enter Your First Name And Last Name..."
+#     formated_f_name = f_name.title()
+#     formated_l_name = l_name.title()
+#     return f"{formated_f_name} {formated_l_name}"
+# formated_string = format_name(input("Enter First Name Here:\n"), input("Enter Last Name Here:\n"))
+# print(formated_string)
+# print(len(formated_string))
+
+# Days In Month Project
+
+# def is_leap(year):
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             return True
+#     else:
+#         return False
+
+# def days_in_month(year, month):
+#     if month > 12 or month < 1:
+#         return "Invalid Month"
+#     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#     if is_leap(year) and month == 2:
+#         return 29
+#     return month_days[month - 1]
+
+# year = int(input("Enter A Year:\n"))
+# month = int(input("Enter A Month:\n"))
+# days = days_in_month(year, month)
+# print(days)
+
+# Doc Strings
+
+# def function_docstring(a, b):
+#     """ This Is A Function Using Dock String."""
+#     return a + b
+# print(function_docstring(2, 3))
+
+# CALCULATOR APP
+
+# import calculator_logo
+
+# print(calculator_logo.logo)
+
+# # Add
+# def add(n1, n2):
+#     return n1 + n2
+
+# # Substract
+# def sub(n1, n2):
+#     return n1 - n2
+
+# # Multiply
+# def mul(n1, n2):
+#     return n1 * n2
+
+# # Division
+# def div(n1, n2):
+#     return n1 / n2
+
+# # Remainder
+# def rem(n1, n2):
+#     return n1 % n2
+
+# # Dictionary
+
+# operations = {
+#     "+": add,
+#     "-": sub,
+#     "*": mul,
+#     "/": div,
+#     "%": rem
+# }
+
+# def calculator():
+    
+#     num1 = float(input("Enter The Number One:\n"))
+
+#     for symbol in operations:
+#         print(symbol)
+
+#     should_continue = True
+
+#     while should_continue:
+
+#         operation_symbol = input("Pick An Operation From The Line Above:\n")
+#         num2 = float(input("Enter The Number Two:\n"))
+#         calculator_function = operations[operation_symbol]
+#         answer = calculator_function(num1, num2)
+
+#         print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+#         if input(f"Type 'y' To Continue With {answer}, Otherwise Type 'n' to exit.:\n") == "y":
+#             num1 = answer
+#         else:
+#             should_continue = False
+
+# calculator()
